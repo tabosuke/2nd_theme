9fca1473b4dbd43ad8c7ef0906d4e17689dbc89e
bamshot <tabosuke810@gmail.com>
2018-08-31 22:45:47 +0900

    初期設定

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <div class="footer-inner">
+
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport"content="width=device-width, initial-scale=1.0 ">
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header>
+  <div class="header-inner">
+
+  </div>
+</header>
